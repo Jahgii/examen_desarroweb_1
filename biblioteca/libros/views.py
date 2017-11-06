3e9e53b0511119d9fab135c0de69c68639e10a5e
Jahgii <jahg.huerta@gmail.com>
2017-11-05 17:31:12 -0800

    Paso 6 y 7 2do examen

--- biblioteca/libros/views.py
+++ biblioteca/libros/views.py
@@ -4,10 +4,11 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import CreateView, UpdateView, DeleteView
+from django.views.generic import CreateView, UpdateView, DeleteView, ListView
 from .forms import LibroModelForm
 from .mixin import FormUserNeededMixin
 from django.urls import reverse_lazy
+from django.db.models import Q
 
 from .models import Libro
 
@@ -30,17 +31,30 @@ class LibroDeleteView(LoginRequiredMixin, DeleteView):
     template_name = "Delete_confirm.html"
     success_url = reverse_lazy("list")
 
+class LibroListView(ListView):
+    template_name = "lista_libros.html"
+
+    def get_queryset(self, *args, **kwargs):
+        qs = Libro.objects.all()
+        print self.request.GET
+        query = self.request.GET.get("q", None)
+        if query is not None:
+            qs = qs.filter(
+            Q(Nombre__icontains=query)|
+            Q(user__username__icontains=query)
+            )
+        return qs
+    def get_context_data(self, *args, **kwargs):
+         context = super(LibroListView, self).get_context_data(*args, **kwargs)
+         print context
+         context['create_form'] = LibroModelForm()
+         context['create_url'] = reverse_lazy("Libro_create")
+         return context
+
 def home(request):
     print request
     Titulo="Libros"
     return render(request, 'home.html', {'TitleV': Titulo})
-
-def lista_libros(request):
-    result_set = Libro.objects.all()
-    context = {
-    "result": result_set
-    }
-    return render(request, "lista_libros.html", context)
 
 def detalle_libro(request, id=1):
     result_set = Libro.objects.get(id=id)
